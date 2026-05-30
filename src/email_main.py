e2db0231899995fe57f77538eb62ca55c6bc893b
ElenaRussu <lenarussu07@gmail.com>
2026-05-30 21:42:40 +0300

    Update email_main.py

--- src/email_main.py
+++ src/email_main.py
@@ -1,1 +1,14 @@
 #экземляр (модель письма) Лена Руссу
+from dataclasses import dataclass
+from typing import Optional
+
+@dataclass
+class Email:
+    message_id: str
+    from_email: str
+    to: str
+    subject: str
+    text: str
+    html: Optional[str] = None
+    date: Optional[str] = None
+    files: list = None
